46211fe22628177bd8ac6107541ca9bbc91e40ce
levin44 <nisaldayan@gmail.com>
2023-09-17 22:38:05 +0530

    notification panel completed

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,6 +1,18 @@
-import { Box} from "@mui/material";
+import { Box, Typography} from "@mui/material";
 import Header from "../components/global/MessageHeader";
+import React from "react";
+import { mockNotifications } from "../data/mockData";
+import { blue } from "@mui/material/colors";
 
+
+const commonStyles = {
+  bgcolor: 'background.paper',
+  
+  m: 1,
+  border: 3,
+  width: '19rem',
+  height: '25rem',
+};
 
 const Dashboard = () => {
   return (
@@ -10,7 +22,51 @@ const Dashboard = () => {
         m="40px 0 0 0"
         height="60vh"
        
-      >       
+      >
+        <Box overflow="auto" sx={{ ...commonStyles, borderRadius: '16px', borderColor: '#84C1FF' }}>
+          <Typography fontSize="17px" color="grey" fontWeight="600" mt="10px" ml="20px" mb="10px">
+            Notifications
+          </Typography>
+
+          {mockNotifications.map((transaction, i)=>(
+                <Box
+                  
+                  display="flex"
+                  alignItems="center"
+                  borderBottom={`4px solid ${blue}`}
+                  p="9px"
+                >
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <img
+                    alt="profile-user"
+                    width="50px"
+                    height="50px"
+                    src={transaction.imageUrl}
+                    style={{ borderRadius: "50%" }}
+                  />
+                </Box>
+
+                  <Box ml="20px">
+                    <Typography
+                      color="black"
+                      variant="h5"
+                      fontWeight="600"
+                    >
+                      {transaction.txId}
+                    </Typography>
+                    <Typography
+                      color="grey"
+                    >
+                      {transaction.timestamp}
+                    </Typography>
+                  </Box>
+                  
+                  
+                </Box>
+              ))}
+
+
+        </Box>       
       </Box>
     </Box>
   );
